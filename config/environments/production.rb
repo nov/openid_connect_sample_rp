eced973c3f0b1c3adc0340ad2b46d9a7ffdda2c4
nov matake <nov@matake.jp>
2012-10-13 14:59:07 +0900

    annoying error... ActionController::RoutingError (No route matches [GET] "/javascripts/application.js")

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ ConnectRp::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.logger = nil
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
