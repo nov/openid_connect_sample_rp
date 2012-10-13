a702e8a66a3aa29a34da63314b4099bb3d1d1431
nov matake <nov@matake.jp>
2012-10-13 15:10:56 +0900

    try this

---
+++ lib/debug_exceptions_ext.rb
@@ -0,0 +1,10 @@
+class ActionDispatch::DebugExceptions
+  alias_method :old_log_error, :log_error
+  def log_error(env, wrapper)
+    if wrapper.exception.is_a?  ActionController::RoutingError
+      return
+    else
+      old_log_error env, wrapper
+    end
+  end
+end
