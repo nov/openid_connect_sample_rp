97175f055f378395f318532bbde4eac0da236163
nov <nov.matake@gree.net>
2014-07-02 12:15:37 +0900

    MS's access token is long..

--- db/migrate/20110829042007_create_open_ids.rb
+++ db/migrate/20110829042007_create_open_ids.rb
@@ -2,8 +2,8 @@ class CreateOpenIds < ActiveRecord::Migration
   def self.up
     create_table :open_ids do |t|
       t.belongs_to :account, :provider
-      t.string :identifier, :access_token
-      t.string :id_token, limit: 1024
+      t.string :identifier
+      t.string :access_token, :id_token, limit: 1024
       t.timestamps
     end
   end
