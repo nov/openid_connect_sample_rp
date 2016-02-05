bab8f08107ac4618e5115ab83646a9754ebea96a
nov <nov@matake.jp>
2016-02-05 14:12:54 +0900

    require session[:nonce] to mitigate CSRF attack at token endpoint

--- lib/nonce.rb
+++ lib/nonce.rb
@@ -1,9 +1,11 @@
 module Nonce
+  class SessionBindingRequired < StandardError; end
+
   def new_nonce
     session[:nonce] = SecureRandom.hex(16)
   end
 
   def stored_nonce
-    session.delete(:nonce)
+    session.delete(:nonce) or raise SessionBindingRequired.new('Invalid Session')
   end
 end
