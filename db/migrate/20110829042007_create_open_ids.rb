640543335fa659d44bdc549271c2d894146e2cdd
Nov Matake <nov@matake.jp>
2017-05-15 11:09:47 +0900

    azure ad access token is longer than 1024 bytes

--- db/migrate/20110829042007_create_open_ids.rb
+++ db/migrate/20110829042007_create_open_ids.rb
@@ -3,7 +3,7 @@ class CreateOpenIds < ActiveRecord::Migration
     create_table :open_ids do |t|
       t.belongs_to :account, :provider
       t.string :identifier
-      t.string :access_token, limit: 1024
+      t.string :access_token, limit: 2048
       t.string :id_token, limit: 2048
       t.timestamps
     end
